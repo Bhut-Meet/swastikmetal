31ad83d8ff6668d4269274634d74f0f04ec48f9b
Bhut-Meet <meetbhut.7272@gmail.com>
2025-04-15 10:51:58 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/swastik-metal/index.php';
+$config['base_url'] = 'https://bhut-meet.github.io/swastikmetal/index.php';
 
 /*
 |--------------------------------------------------------------------------
